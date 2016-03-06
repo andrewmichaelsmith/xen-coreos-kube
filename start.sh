6d45c66a30807945a302160705f91ec10fcd084b
Andrew Smith <me@andrewmichaelsmith.com>
2016-03-06 19:25:00 +0000

    Nodes were sharing file systems
    copy/paste error

--- start.sh
+++ start.sh
@@ -35,7 +35,7 @@ virt-install \
   --os-type=linux \
   --os-variant=virtio26 \
   --disk path=/var/lib/libvirt/images/coreos/node2.qcow2,format=qcow2,bus=virtio \
-  --filesystem /var/lib/libvirt/images/coreos/node1/,config-2,type=mount,mode=squash \
+  --filesystem /var/lib/libvirt/images/coreos/node2/,config-2,type=mount,mode=squash \
   --network bridge=virbr0,mac=52:54:00:00:00:1 \
   --vnc \
   --noautoconsole
@@ -49,7 +49,7 @@ virt-install \
   --os-type=linux \
   --os-variant=virtio26 \
   --disk path=/var/lib/libvirt/images/coreos/node3.qcow2,format=qcow2,bus=virtio \
-  --filesystem /var/lib/libvirt/images/coreos/node1/,config-2,type=mount,mode=squash \
+  --filesystem /var/lib/libvirt/images/coreos/node3/,config-2,type=mount,mode=squash \
   --network bridge=virbr0,mac=52:54:00:00:00:2 \
   --vnc \
   --noautoconsole
