0518dee333ccb254934e33d297f1b1bd7e378009
Andrew Smith <me@andrewmichaelsmith.com>
2016-04-06 21:20:10 +0100

    Hack

--- certs/gen.sh
+++ certs/gen.sh
@@ -4,3 +4,7 @@ openssl genrsa -out apiserver-key.pem 2048
 openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config openssl.cnf
 openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile openssl.cnf
 
+#Total hack
+#So it's indented correctly when we move it in to .yml
+sed -i 's/^/        /' *.pem
+
