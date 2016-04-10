ec1e0e5a892a8a1c41d7ef7f34780b79d9898846
Andrew Smith <me@andrewmichaelsmith.com>
2016-04-10 11:42:02 +0100

    Delete ssh keys when you throw disks away

--- recreate-disks.sh
+++ recreate-disks.sh
@@ -8,4 +8,7 @@ qemu-img create -f qcow2 -b coreos_production_qemu_image.img node1.qcow2
 qemu-img create -f qcow2 -b coreos_production_qemu_image.img node2.qcow2
 qemu-img create -f qcow2 -b coreos_production_qemu_image.img node3.qcow2
 
-
+ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.122.254
+ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.122.2
+ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.122.3
+ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.122.4
